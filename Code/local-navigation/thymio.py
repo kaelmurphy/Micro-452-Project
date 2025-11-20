d3ca5e5a7e8e8f7d74847bff50d26c68a6fd6ed4
Killian Baillifard <killian.baillifard@gmail.com>
2025-11-20 02:38:56 +0100

    Made obstacle avoidance more robust

--- Code/local-navigation/thymio.py
+++ Code/local-navigation/thymio.py
@@ -6,6 +6,7 @@
 from tdmclient import ClientAsync, aw
 from tdmclient.clientasyncnode import ClientAsyncNode
 from time import perf_counter
+from enum import Enum
 import numpy as np
 
 # Functions
@@ -13,10 +14,6 @@ def wrap(radians: float) -> float:
     return (radians + np.pi) % (2 * np.pi) - np.pi
 
 def split_int32(x: int) -> tuple[int, int]:
-    """
-    Split a 32 bit integer into its 16 bits high and low parts 
-    """
-
     x &= 0xFFFFFFFF
     lo = x & 0xFFFF
     if lo >= 0x8000:
@@ -27,10 +24,6 @@ def split_int32(x: int) -> tuple[int, int]:
     return hi, lo
     
 def combine_int32(hi: int, lo: int) -> int:
-    """
-    Combine both 16 bits high and low parts of a 32 bit integer 
-    """
-
     hi &= 0xFFFF
     lo &= 0xFFFF
     x = (hi << 16) | lo
@@ -38,20 +31,47 @@ def combine_int32(hi: int, lo: int) -> int:
         x -= 0x100000000
     return x
 
+def cross2d(a: np.ndarray, b: np.ndarray):
+    return a[0] * b[1] - a[1] * b[0]
+
+def trajectory_direction(x: float, y: float, path: np.ndarray) -> int:
+    if path.shape[0] >= 3:
+        p0 = np.array([x, y])
+        p1 = path[0]
+        p2 = path[1]
+        return -1 if cross2d(p1 - p0, p2 - p1) < 0 else 1
+    else:
+        return 1
+
+def segments_intersection_distance(s1: np.ndarray, s2: np.ndarray) -> float | None:
+    r = s1[1] - s1[0]
+    s = s2[1] - s2[0]
+    r_cross_s = cross2d(r, s)
+    if abs(r_cross_s) < 1e-9:
+        return None
+    q_minus_p = s2[0] - s1[0]
+    t = cross2d(q_minus_p, s) / r_cross_s
+    u = cross2d(q_minus_p, r) / r_cross_s
+    if 0 <= t <= 1 and 0 <= u <= 1:
+        seg_len = np.linalg.norm(r)
+        if seg_len < 1e-12:
+            return None
+        return t * seg_len
+    return None
+
+def path_intersection_distance(path: np.ndarray, segment: np.ndarray) -> tuple[float | None, int | None]:
+    for i in range(len(path) - 1):
+        pathSegment = path[i:(i + 2)]
+        d = segments_intersection_distance(segment, pathSegment)
+        if d is not None:
+            return d, i
+    return None, None
+
 class Calibration():
-    """
-    Calibration class for a Thymio robot
-
-    Two values must be calibrated:
-
-        - scale : um per steps (um/lsb)
-
-        - track : Wheel track (mm)
-    """
 
     def __init__(self, scale: float, track: float) -> None:
-        self.scale = scale
-        self.track = track
+        self.scale = scale # (um/lsb)
+        self.track = track # (mm)
 
     def mm_to_steps(self, millimeters: float) -> int:
         return int(np.round(1000 * np.abs(millimeters) / self.scale))
@@ -102,7 +122,8 @@ class Thymio():
         error = aw(self.node.register_events([
             ('done', 4),
             ('obstructed', 4),
-            ('clear', 4),
+            ('cleared', 4),
+            ('probed', 4),
             ('position', 4)
         ]))
         if error is not None:
@@ -185,10 +206,8 @@ class Thymio():
         millimeters = np.linalg.norm(vector)
         return millimeters, radians
 
-    def forward(self, millimeters: float) -> tuple[str, list[int]]:
+    def forward(self, millimeters: float) -> None:
         print(f'Moving {millimeters} millimeters')
-        if millimeters <= 0:
-            return 'reached', None
         l_hi, l_lo = split_int32(self.cal.mm_to_steps(millimeters))
         r_hi, r_lo = split_int32(self.cal.mm_to_steps(millimeters))
         self.state = 'forward'
@@ -202,10 +221,8 @@ class Thymio():
             R_R_LO = r_lo
         )
 
-    def turn(self, radians: float) -> tuple[str, list[int]]:
+    def turn(self, radians: float) -> None:
         print(f'Turning {radians:.3f} radians')
-        if radians == 0:
-            return 'reached', None
         l_hi, l_lo = split_int32(self.cal.radians_to_steps(radians))
         r_hi, r_lo = split_int32(self.cal.radians_to_steps(radians))
         self.state = 'right turn' if np.sign(radians) > 0 else 'left turn'
@@ -217,6 +234,22 @@ class Thymio():
             R_L_LO = l_lo,
             R_R_HI = r_hi,
             R_R_LO = r_lo
+        )
+
+    def clear(self, direction: int) -> None:
+        self.state = 'right turn' if direction > 0 else 'left turn'
+        self.execute(
+            'clear.aesl',
+            DIR_L = -direction,
+            DIR_R = direction
+        )
+
+    def probe(self, direction: int) -> None:
+        self.state = 'right turn' if direction > 0 else 'left turn'
+        self.execute(
+            'probe.aesl',
+            DIR_L = -direction,
+            DIR_R = direction
         )
 
 # Robots calibrations
@@ -252,35 +285,161 @@ def create_square_path(side: float, turns: int) -> np.ndarray:
             square.append([0.0, side])
     return np.array(square)
 
+def create_rect_path(width: float, height: float, turns: int) -> np.ndarray:
+    rect = []
+    for _ in range(turns):
+            rect.append([0.0, 0.0])
+            rect.append([width, 0.0])
+            rect.append([width, height])
+            rect.append([0.0, height])
+    return np.array(rect)
+
 def follow_path(theta0: float, path: np.ndarray):
+
+    class State(Enum):
+        FACE        = 0
+        REACH       = 1
+        TURN_AWAY   = 2
+        EXIT_PATH   = 3
+        PROBE       = 4
+        CLEAR       = 5
+        NUDGE       = 6
+
+    THYMIO_LENGTH = 110
 
     # Connect Thymio
     with Thymio(THYMIO_482_CALIBRATION) as thymio:
+
+        # Initialize state machine shared variables
+        state: State = None
+        millimeters: float = None
+        radians: float = None
+        avoidanceDirection: int = None
+        index: int = None
+            
+        # State transitions
+        def next(i: int) -> None:
+            nonlocal path
+            if path.size > i:
+                path = path[i:]
+            else:
+                path = np.array([])
+
+        def face() -> None:
+            nonlocal thymio, path, state, millimeters, radians
+            millimeters, radians = thymio.distance_and_angle_to(path[1])
+            thymio.turn(radians)
+            state = State.FACE
+
+        def reach() -> None:
+            nonlocal thymio, state, millimeters
+            thymio.forward(millimeters)
+            state = State.REACH
+        
+        def turn_away() -> None:
+            nonlocal thymio, path, state, avoidanceDirection
+            avoidanceDirection = -trajectory_direction(thymio.x, thymio.y, path)
+            thymio.clear(avoidanceDirection)
+            state = State.TURN_AWAY
+
+        def exit_path() -> None:
+            nonlocal thymio, state
+            thymio.forward(THYMIO_LENGTH)
+            state = State.EXIT_PATH
+
+        def probe() -> None:
+            nonlocal thymio, state, avoidanceDirection
+            thymio.probe(-avoidanceDirection)
+            state = State.PROBE
+
+        def clear() -> None:
+            nonlocal thymio, state, avoidanceDirection
+            thymio.clear(avoidanceDirection)
+            state = State.CLEAR
+
+        def nudge() -> None:
+            nonlocal thymio, path, index, state
+            segment = np.array([
+                [
+                    thymio.x,
+                    thymio.y
+                ],
+                [
+                    thymio.x + THYMIO_LENGTH * np.cos(thymio.theta),
+                    thymio.y + THYMIO_LENGTH * np.sin(thymio.theta)
+                ]
+            ])
+            distance, index = path_intersection_distance(path, segment)
+            thymio.forward(distance if distance is not None else THYMIO_LENGTH)
+            state = State.NUDGE
 
         # Set initial coordinates and angle
         thymio.x = path[0][0]
         thymio.y = path[0][1]
         thymio.theta = theta0
+        face()
 
-        # For each waypoint
-        for waypoint in path[1:]:
-            millimeters, radians = thymio.distance_and_angle_to(waypoint)
+        # Run state machine
+        while path.size > 0:
 
-            # Turn toward waypoint
-            thymio.turn(radians)
-            while thymio.state == 'right turn':
-                aw(thymio.client.sleep(0.5))
-                thymio.print_pose()
+            # Run Thymio local navigation
+            match state:
 
-            # Move to waypoint
-            thymio.forward(millimeters)
-            while thymio.state == 'forward':
-                aw(thymio.client.sleep(0.5))
-                thymio.print_pose()
+                case State.FACE:
+                    match thymio.state:
+                        case 'done':
+                            reach()
+                        case 'obstructed':
+                            turn_away()
+                    
+                case State.REACH:
+                    match thymio.state:
+                        case 'done':
+                            next(1)
+                            face()
+                        case 'obstructed':
+                            turn_away()
+                    
+                case State.TURN_AWAY:
+                    match thymio.state:
+                        case 'cleared':
+                            exit_path()
+
+                case State.EXIT_PATH:
+                    match thymio.state:
+                        case 'done':
+                            probe()
+                        case 'obstructed':
+                            clear()
+
+                case State.PROBE:
+                    match thymio.state:
+                        case 'probed':
+                            clear()
+
+                case State.CLEAR:
+                    match thymio.state:
+                        case 'cleared':
+                            nudge()
+
+                case State.NUDGE:
+                    match thymio.state:
+                        case 'done':
+                            if index is not None:
+                                next(index)
+                                face()
+                            else:
+                                probe()
+                        case 'obstructed':
+                            clear()
+
+            # TODO Call Kalman filter and do decision making
+            aw(thymio.client.sleep(0.5))
+            # thymio.print_pose()
 
 if __name__ == '__main__':
 
     try:
-        follow_path(0, create_square_path(200, 4))
+        follow_path(0, create_rect_path(200, 600, 4))
     except KeyboardInterrupt:
         pass
