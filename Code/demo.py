c6a5d88ddc39d8f8b555c5fa310fa2b219e0272d
Killian Baillifard <killian.baillifard@gmail.com>
2025-11-28 17:34:20 +0100

    Implemented mutlithreaded demo

--- Code/demo.py
+++ Code/demo.py
@@ -2,56 +2,48 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from enum import Enum
-from tdmclient import aw
 from localnav import *
 from globalnav import *
 from Kalman import *
 from globalnav_plot import setup_globalnav_plot, ARROW_LENGTH
-from vision import getVisionCoords
 from time import perf_counter
+from vision import getVisionCoords
+from threading import Thread
+from time import sleep
 
-def main():
+# Testing settings
 
-    # Settings
-    NO_THYMIO_MODE = False
-    NO_CAMERA_MODE = False
-    Q = np.diag([0.005, 0.005, 0.002])
-    R_cam = np.diag([0.002, 0.002, 0.0005])
-    P = np.diag([1.0, 1.0, 0.5])
-    TARGET_TOLERANCE = 30
+NO_THYMIO_MODE = False
+NO_CAMERA_MODE = True
 
-    # Capture first image
-    if NO_CAMERA_MODE:
-        df = pd.read_csv("globalnav/CSV_Data/Simulation_Data_VG_V1.0_13.11.25.csv", sep=";")
-        coords = df[["type", "id", "label", "x", "y"]].to_numpy(dtype=object)
-        theta0 = 0
-    else:
-        coords, theta0 = getVisionCoords(timeout=None, showDisplay=True)
-        print(coords, "Initial orientation: {:.2f}".format(theta0))
+# Navigation settings (mm)
 
-    # Compute best path
-    path = compute_global_path(coords, epsilon_mm=65.0)
-    print("A* global path:\n", path)
+GLOB_NAV_EPSILON = 65
+ERROR_TOLERANCE = 30
 
-    # Global nav plot: map + polygons + A* path + empty odometry line
-    global_path, debug, fig, ax, odom_line, odom_arrow, cam_line, cam_arrow = setup_globalnav_plot(
-        coords,
-        epsilon_mm=50.0,
-        show_neighbors=False,
-        initial_theta=theta0,
-    )
+# Kalman settings
 
-    # ---------------------------------------------------------
-    # SIMULATION-ONLY MODE (NO THYMIO)
-    # ---------------------------------------------------------
-    if NO_THYMIO_MODE:
-        print("SIMULATION MODE ENABLED - no Thymio, showing static map + arrow.")
-        plt.ioff()
-        plt.show()
-        return
+TS = 0.1
+Q = np.diag([0.005, 0.005, 0.002])
+R_cam = np.diag([0.002, 0.002, 0.0005])
+P = np.diag([1.0, 1.0, 0.5])
 
+# Globals
 
+thymio: Thymio = None
+camPose: np.ndarray = np.zeros(3)
+newCamPose: bool = None
+
+# Threads
+
+def thymio_thread(path: np.ndarray, theta0: float) -> None:
+
+    # Shared globals
+
+    global ERROR_TOLERANCE, TS, Q, R_cam, P, thymio, camPose, newCamPose
+    
     # Local navigation state machine
+
     class State(Enum):
         FOLLOW          = 0
         FACE_AWAY       = 1
@@ -61,165 +53,216 @@ def main():
         END             = 5
 
     # Connect Thymio
-    with Thymio(THYMIO_482_CALIBRATION) as thymio:
 
-        # Set initial position
-        thymio.set_pose(path[0][0], path[0][1], theta0)
+    with Thymio(path[0][0], path[0][1], theta0, THYMIO_482_CALIBRATION) as thymio:
 
-        # Initialize state machine
+        # Initialize motor control and local navigation
+
         thymio.set_target(path[1][0], path[1][1])
         state: State = State.FOLLOW
-        lastTime = perf_counter()
+        t0 = perf_counter()
 
         # Until path is completed
-        try:
-            # Until path is completed
-            while state != State.END:
 
-                lastDebug = perf_counter()
+        while state != State.END:
 
-                # Local navigation
-                match state:
+            # Wait one sampling period
 
-                    case State.FOLLOW:
-                        if np.linalg.norm([path[1][0] - thymio.x, path[1][1] - thymio.y]) < TARGET_TOLERANCE:
-                            if path.shape[0] > 2:
-                                path = path[1:]
-                                thymio.set_target(path[1][0], path[1][1])
-                            else:
-                                state = State.END
-                        elif thymio.is_blocked():
-                            thymio.probe_obstacle(path)
-                            state = State.FACE_AWAY
+            t1 = perf_counter()
+            dt = t1 - t0
+            t0 = t1
+            aw(thymio.client.sleep(max([0.001, TS - dt])))
 
-                    case State.FACE_AWAY:
-                        if thymio.is_clear():
-                            THYMIO_LENGTH = 110
-                            x = int(thymio.x + THYMIO_LENGTH * np.cos(thymio.theta))
-                            y = int(thymio.y + THYMIO_LENGTH * np.sin(thymio.theta))
+            # Acquire sample
+            
+            thymioPose = np.array([thymio.x, thymio.y, thymio.theta])
+            v = thymio.v
+            omega = thymio.omega
+
+            # Run local navigation (avoidance) state machine
+
+            match state:
+
+                case State.FOLLOW:
+                    if np.linalg.norm([path[1][0] - thymioPose[0], path[1][1] - thymioPose[1]]) < ERROR_TOLERANCE:
+                        if path.shape[0] > 2:
+                            path = path[1:]
+                            thymio.set_target(path[1][0], path[1][1])
+                        else:
+                            state = State.END
+                    elif thymio.is_blocked():
+                        thymio.probe_obstacle(path)
+                        state = State.FACE_AWAY
+
+                case State.FACE_AWAY:
+                    if thymio.is_clear():
+                        THYMIO_LENGTH = 110
+                        x = int(thymioPose[0] + THYMIO_LENGTH * np.cos(thymioPose[2]))
+                        y = int(thymioPose[1] + THYMIO_LENGTH * np.sin(thymioPose[2]))
+                        thymio.set_target(x, y)
+                        state = State.EXIT_PATH
+
+                case State.EXIT_PATH:
+                    if np.linalg.norm([x - thymioPose[0], y - thymioPose[1]]) < ERROR_TOLERANCE:
+                        thymio.probe_obstacle(path)
+                        state = State.PROBE_OBSTACLE
+
+                case State.PROBE_OBSTACLE:
+                    if thymio.is_clear():
+                        THYMIO_LENGTH = 110
+                        x = int(thymioPose[0] + THYMIO_LENGTH * np.cos(thymioPose[2]))
+                        y = int(thymioPose[1] + THYMIO_LENGTH * np.sin(thymioPose[2]))
+                        segment = np.array([[thymioPose[0], thymioPose[1]], [x, y]])
+                        intersect, index = path_intersection_point(path, segment)
+                        if index is not None:
+                            thymio.set_target(intersect[0], intersect[1])
+                        else:
                             thymio.set_target(x, y)
-                            state = State.EXIT_PATH
+                        state = State.NUDGE_FORWARD
 
-                    case State.EXIT_PATH:
-                        if np.linalg.norm([x - thymio.x, y - thymio.y]) < TARGET_TOLERANCE:
+                case State.NUDGE_FORWARD:
+                    if index is not None:
+                        if np.linalg.norm([intersect[0] - thymioPose[0], intersect[1] - thymioPose[1]]) < ERROR_TOLERANCE:
+                            path = path[index:]
+                            thymio.set_target(path[1][0], path[1][1])
+                            state = State.FOLLOW
+                    else:
+                        if np.linalg.norm([x - thymioPose[0], y - thymioPose[1]]) < ERROR_TOLERANCE:
                             thymio.probe_obstacle(path)
                             state = State.PROBE_OBSTACLE
 
-                    case State.PROBE_OBSTACLE:
-                        if thymio.is_clear():
-                            THYMIO_LENGTH = 110
-                            x = int(thymio.x + THYMIO_LENGTH * np.cos(thymio.theta))
-                            y = int(thymio.y + THYMIO_LENGTH * np.sin(thymio.theta))
-                            segment = np.array([[thymio.x, thymio.y], [x, y]])
-                            intersect, index = path_intersection_point(path, segment)
-                            if index is not None:
-                                thymio.set_target(intersect[0], intersect[1])
-                            else:
-                                thymio.set_target(x, y)
-                            state = State.NUDGE_FORWARD
+            # Run Kalman filter
 
-                    case State.NUDGE_FORWARD:
-                        if index is not None:
-                            if np.linalg.norm([intersect[0] - thymio.x, intersect[1] - thymio.y]) < TARGET_TOLERANCE:
-                                path = path[index:]
-                                thymio.set_target(path[1][0], path[1][1])
-                                state = State.FOLLOW
-                        else:
-                            if np.linalg.norm([x - thymio.x, y - thymio.y]) < TARGET_TOLERANCE:
-                                thymio.probe_obstacle(path)
-                                state = State.PROBE_OBSTACLE
+            estimatedPose, P = ekf_step(thymioPose, P, v, omega, camPose, newCamPose, TS, Q, R_cam)
+            newCamPose = False
 
-                newDebug = perf_counter()
-                localNavTime = newDebug - lastDebug
-                lastDebug = newDebug
+            # Update position only if off by more than 30 mm (avoid unnecessary rollback)
 
-                # TODO Camera acquisition
-                coords, theta = getVisionCoords(timeout=None, showDisplay=False)
-                # --- Update camera trajectory & arrow (green) ---
-                try:
-                    # camera measurement in world (mm)
-                    x_cam = coords[4][3]
-                    y_cam = coords[4][4]
+            dx = np.abs(estimatedPose[0] - thymioPose[0])
+            dy = np.abs(estimatedPose[1] - thymioPose[1])
+            if dx > 30 or dy > 30:
+                thymio.set_pose(estimatedPose[0], estimatedPose[1], estimatedPose[2])
 
-                    # 1) camera trajectory line
-                    xs_cam, ys_cam = cam_line.get_data()
-                    xs_cam = list(xs_cam) + [x_cam]
-                    ys_cam = list(ys_cam) + [y_cam]
-                    cam_line.set_data(xs_cam, ys_cam)
+        # Stop thymio
 
-                    # 2) camera arrow (using camera heading 'theta' from vision)
-                    x_head_cam = x_cam + ARROW_LENGTH * np.cos(theta)
-                    y_head_cam = y_cam + ARROW_LENGTH * np.sin(theta)
-                    cam_arrow.set_positions((x_cam, y_cam), (x_head_cam, y_head_cam))
+        thymio.node.stop()
 
-                except Exception as e:
-                    # if coords are not in the expected shape, just skip this frame
-                    print("Camera plotting error:", e)
+def camera_thread() -> None:
 
-                newDebug = perf_counter()
-                camAcquisitionTime = newDebug - lastDebug
-                lastDebug = newDebug
+    # Globals
 
-                # Run Kalman filter
+    global camPose, newCamPose
+    
+    while True:
 
-                newTime = perf_counter()
-                elapsed = newTime - lastTime
-                lastTime = newTime
+        coords, theta = getVisionCoords(timeout=None, showDisplay=False)
+        camPose = np.array([coords[4][3], coords[4][4], theta])
+        newCamPose = True
 
-                z_cam = np.array([coords[4][3], coords[4][4], theta])
-                x_prev = np.array([thymio.x, thymio.y, thymio.theta])
-                x_new, P = ekf_step(x_prev, P, thymio.v, thymio.omega, z_cam, True, elapsed, Q, R_cam)
+def main_thread() -> None:
 
-                newDebug = perf_counter()
-                kalmanTime = newDebug - lastDebug
-                lastDebug = newDebug
+    # Globals
 
-                # Update position with new estimation
-                thymio.set_pose(x_new[0], x_new[1], x_new[2])
+    global NO_CAMERA_MODE, NO_THYMIO_MODE, GLOB_NAV_EPSILON, thymio, camPose
 
-                newDebug = perf_counter()
-                posUpdateTime = newDebug - lastDebug
-                lastDebug = newDebug
-                
-                # -------------------------------------------------
-                # UPDATE MATPLOTLIB PLOT (ODOMETRY + ARROW)
-                # -------------------------------------------------
-                # 1) Update odometry trail
-                xs, ys = odom_line.get_data()
-                xs = list(xs) + [thymio.x]
-                ys = list(ys) + [thymio.y]
-                odom_line.set_data(xs, ys)
+    # Use simulation map if camera is not available
 
-                # 2) Update odometry arrow
-                x = thymio.x
-                y = thymio.y
-                theta_est = thymio.theta
+    if NO_CAMERA_MODE:
+        df = pd.read_csv("simulation.csv", sep=";")
+        coords = df[["type", "id", "label", "x", "y"]].to_numpy(dtype=object)
+        theta0 = 0
 
-                x_head = x + ARROW_LENGTH * np.cos(theta_est)
-                y_head = y + ARROW_LENGTH * np.sin(theta_est)
-                odom_arrow.set_positions((x, y), (x_head, y_head))
+    # Else capture first image
 
-                fig.canvas.draw()
-                fig.canvas.flush_events()
+    else:
+        coords, theta0 = getVisionCoords(timeout=None, showDisplay=True)
+        print(coords, "Initial orientation: {:.2f}".format(theta0))
 
-                newDebug = perf_counter()
-                plotTime = newDebug - lastDebug
-                lastDebug = newDebug
+    # Compute best path
 
-                print(f'localNavTime {localNavTime:.3f} camAcquisitionTime {camAcquisitionTime:.3f} kalmanTime {kalmanTime:.3f} posUpdateTime {posUpdateTime:.3f} plotTime {plotTime:.3f}')
+    path = compute_global_path(coords, epsilon_mm=GLOB_NAV_EPSILON)
+    print("A* global path:\n", path)
 
-                # Pace loop at 20 ms
-                aw(thymio.client.sleep(0.02))
+    # Global nav plot: map + polygons + A* path + empty odometry line
+
+    global_path, debug, fig, ax, odom_line, odom_arrow, cam_line, cam_arrow = setup_globalnav_plot(
+        coords,
+        epsilon_mm=50.0,
+        show_neighbors=False,
+        initial_theta=theta0,
+    )
+
+    # Only plot global nav path if thymio is not available
+
+    if NO_THYMIO_MODE:
+        print("SIMULATION MODE ENABLED - no Thymio, showing static map + arrow.")
+        plt.ioff()
+        plt.show()
+        return
+    
+    # Only start camera thread if camera is available
+
+    if not NO_CAMERA_MODE:
+        cameraThread = Thread(target=camera_thread, daemon=True)
+        cameraThread.start()
+
+    # Start thymio thread
+
+    thymioThread = Thread(target=thymio_thread, args=(path, theta0), daemon=True)
+    thymioThread.start()
+    while thymio is None:
+        plt.pause(0.001)
+
+    # Run until window is closed
+
+    closed = False
+
+    def on_close(event):
+        nonlocal closed
+        closed = True
+
+    fig.canvas.mpl_connect('close_event', on_close)
+
+    while not closed:
+
+        # Plot camera detected trajectory 
+
+        xs_cam, ys_cam = cam_line.get_data()
+        xs_cam = list(xs_cam) + [camPose[0]]
+        ys_cam = list(ys_cam) + [camPose[1]]
+        cam_line.set_data(xs_cam, ys_cam)
+
+        # Plot camera detected heading 
+
+        x_head_cam = camPose[0] + ARROW_LENGTH * np.cos(camPose[2])
+        y_head_cam = camPose[1] + ARROW_LENGTH * np.sin(camPose[2])
+        cam_arrow.set_positions((camPose[0], camPose[1]), (x_head_cam, y_head_cam))
         
-        finally:
-            print("Exiting control loop.")
+        # Plot odometry detected trajectory 
 
-    plt.ioff()
-    plt.show()
+        xs, ys = odom_line.get_data()
+        xs = list(xs) + [thymio.x]
+        ys = list(ys) + [thymio.y]
+        odom_line.set_data(xs, ys)
+
+        # Plot odometry detected heading 
+
+        x = thymio.x
+        y = thymio.y
+        theta_est = thymio.theta
+        x_head = x + ARROW_LENGTH * np.cos(theta_est)
+        y_head = y + ARROW_LENGTH * np.sin(theta_est)
+        odom_arrow.set_positions((x, y), (x_head, y_head))
+
+        # Update figure
+
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        plt.pause(0.025)
 
 if __name__ == '__main__':
 
     try:
-        main()
+        main_thread()
     except KeyboardInterrupt:
         pass
